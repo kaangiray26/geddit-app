a3fb257f94c07f74a0668e0d471b1238e155da65
kaangiray26 <kaangiray26@protonmail.com>
2023-07-25 13:33:53 +0200

    preparing for v1.6

--- src/components/UpdateManager.vue
+++ src/components/UpdateManager.vue
@@ -27,7 +27,7 @@ import { ref, onMounted, onBeforeUnmount, onBeforeMount } from 'vue';
 import { AppLauncher } from '@capacitor/app-launcher';
 import { Modal } from "bootstrap"
 
-const version = "v1.5";
+const version = "v1.6";
 
 const modal = ref(null);
 const body = ref(null);
