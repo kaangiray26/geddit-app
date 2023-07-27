ee6f83d6d16d0e2fb4896214afcc83581424ec3b
kaangiray26 <kaangiray26@protonmail.com>
2023-07-28 00:26:13 +0200

    ctrees and image zoom #38

--- src/contents/FullImage.vue
+++ src/contents/FullImage.vue
@@ -1,5 +1,8 @@
 <template>
-    <img :src="image_options.preview" loading="lazy">
+    <div class="zoom" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend"
+        :style="`transform: translate3d(${style.x}px, ${style.y}px, 0) scale(${style.scale})`">
+        <img ref="image" class="position-relative z-1" :src="image_options.preview" loading="lazy">
+    </div>
 </template>
 
 <script setup>
@@ -13,7 +16,50 @@ const props = defineProps({
     }
 })
 
+const style = ref({ x: 0, y: 0, scale: 1 });
+const start = ref({ x: 0, y: 0, distance: 0 });
+
+const image = ref(null);
 const image_options = ref({});
+
+const distance = (event) => {
+    return Math.hypot(event.touches[0].clientX - event.touches[1].clientX, event.touches[0].clientY - event.touches[1].clientY);
+};
+
+const midpoint_x = (event) => {
+    return (event.touches[0].clientX + event.touches[1].clientX) / 2;
+};
+
+const midpoint_y = (event) => {
+    return (event.touches[0].clientY + event.touches[1].clientY) / 2;
+};
+
+async function touchstart(event) {
+    if (event.touches.length === 2) {
+        event.preventDefault();
+
+        start.value.x = midpoint_x(event);
+        start.value.y = midpoint_y(event);
+        start.value.distance = distance(event);
+    }
+}
+
+async function touchmove(event) {
+    if (event.targetTouches.length === 2) {
+        event.preventDefault();
+
+        let scale = distance(event) / start.value.distance;
+
+        style.value.scale = scale;
+        style.value.x = start.value.x - (start.value.x * scale) + (midpoint_x(event) - start.value.x);
+        style.value.y = start.value.y - (start.value.y * scale) + (midpoint_y(event) - start.value.y);
+    }
+}
+
+async function touchend() {
+    // Reset image to it's original format
+    style.value = { x: 0, y: 0, scale: 1 };
+}
 
 async function get_sources() {
     if (!props.data.preview) {
